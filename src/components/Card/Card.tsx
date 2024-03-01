e15d30b72283ae4c7e9a9e064f7a4ec3aba2e719
wicho37 <willihernandezramirez@gmail.com>
2024-02-29 22:34:04 -0500

    se inicia a darle forma con el css.modules

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,10 +1,14 @@
+import style from "./Card.module.css"
+
+
+
 
 const Card = () => {
   return (
     <div>
-      <button>
-        <p>try it free 7days</p>
-        <p>then $20/mo. there after</p>
+      <button className={style.press}>
+        <p className={style.bond}>try it free 7days</p>
+        <p className={style.bond1}>then $20/mo. there after</p>
       </button>
     </div>
   )
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,12 @@
+import style from "./Header.module.css"
+
+
 
 const Header = () => {
   return (
-    <div>
-      <p>learn to code by watching others</p>
-      <p>see how experenced developers solve problems in real-time. watching scripted tutorials is great, but understanding how developers thinsk is invaluable.</p>
+    <div className={style.header}>
+      <p className={style.first_Tittle}>learn to code by watching others</p>
+      <p className={style.last_Tittle}>see how experenced developers solve problems in real-time. watching scripted tutorials is great, but understanding how developers thinsk is invaluable.</p>
     </div>
   )
 }
