2867d1118b2b224e093a737748c91232d91caf0c
wicho37 <willihernandezramirez@gmail.com>
2024-03-02 08:46:19 -0500

    se añade mas modules.css al header y parte del form

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -2,13 +2,11 @@ import style from "./Card.module.css"
 
 
 
-
 const Card = () => {
   return (
-    <div>
+    <div >
       <button className={style.press}>
-        <p className={style.bond}>try it free 7days</p>
-        <p className={style.bond1}>then $20/mo. there after</p>
+        <p className={style.bond}>try it free 7days then $20/mo. there after</p>
       </button>
     </div>
   )
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,7 +5,10 @@ import style from "./Header.module.css"
 const Header = () => {
   return (
     <div className={style.header}>
-      <p className={style.first_Tittle}>learn to code by watching others</p>
+      <div className={style.first_Tittle}>
+        <p>learn to code by </p>
+        <p>watching others</p>
+      </div>
       <p className={style.last_Tittle}>see how experenced developers solve problems in real-time. watching scripted tutorials is great, but understanding how developers thinsk is invaluable.</p>
     </div>
   )
